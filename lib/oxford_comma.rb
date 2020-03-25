483bdd178409e2c743f594513b18fdd0a525753e
Ray Alva <rayalva407@gmail.com>
2020-03-25 21:39:44 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,9 +4,8 @@ def oxford_comma(array)
   elsif array.length == 2
     array.join(" and ")
   elsif array.length == 3
-    string = array[0..1].join(", ") << ", and #{array[-1]}"
+    array[0..1].join(", ") << ", and #{array[-1]}"
   else
     array[0..-2].join(", ") << ", and #{array[-1]}"
-    
   end
 end
