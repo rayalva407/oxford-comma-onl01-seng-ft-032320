6ec850b474068e6189b8f48180077671b417c7a2
Ray Alva <rayalva407@gmail.com>
2020-03-25 21:33:10 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -4,6 +4,6 @@ def oxford_comma(array)
   elsif array.length == 2
     array.join(" and ")
   elsif array.length == 3
-    string = array[0..2].join(", ")
+    string = array[0..-1].join(", ")
   end
 end
